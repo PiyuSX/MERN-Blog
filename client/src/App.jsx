48bd6d5620abb683e3e9f284be63f0d78864fcfa
PiyuSX <iam@jrpiyush.me>
2025-10-09 08:34:31 +0545

    Routing & Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+       </Routes>
+    </BrowserRouter>
   )
 }
 
